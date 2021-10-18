223c7a43c9851345732830f370d4bc2e6aeb4793
Rishit Pandey <rishpandey8097@gmail.com>
2021-10-18 21:48:07 +0000

    change looks

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,7 @@
 const Fonts = () => (
-  <style jsx global>{`
-    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
-  `}</style>
+  <style>
+    @import
+    url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap');
+  </style>
 )
 export default Fonts
