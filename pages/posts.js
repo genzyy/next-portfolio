e5c366a389db47b620ca60685e685093b522f1e8
Rishit Pandey <rishpandey8097@gmail.com>
2021-10-23 15:46:03 +0530

    add new posts

--- pages/posts.js
+++ pages/posts.js
@@ -3,11 +3,6 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
-import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
-import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
-import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
-import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
 import fishWorkflow from '../public/images/terminal.png'
 import neovimThumbnail from '../public/images/neovim.png'
 
