0b6ba4fa7826813f8882d3f924e53a4cdd66848a
Rishit Pandey <rishpandey8097@gmail.com>
2022-01-09 20:58:39 +0530

    Add: predli experience

--- pages/experience.js
+++ pages/experience.js
@@ -1,33 +1,53 @@
-import { Container, Heading, SimpleGrid} from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
 import scouttTechThumbnail from '../public/images/experience/scouttTech.png'
 import caarsyThumbnail from '../public/images/experience/caarsy.png'
+import predliThumbnail from '../public/images/experience/predli.png'
 
 const Experience = () => {
-    return (
-        <Layout title="Experience">
-            <Container>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Experience
-                </Heading>
-                <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                    <Section>
-                        <WorkGridItem id="caarsy" title="Caarsy(startup)" thumbnail={caarsyThumbnail}>
-                            India{"'"}s first inventory management software for second hand vehicle dealerships.
-                        </WorkGridItem>
-                    </Section>
-                    <Section>
-                        <WorkGridItem id="scouttTech" title="Scoutt-Tech" thumbnail={scouttTechThumbnail}>
-                            Helping recruiters from early startups to get the best developers across the country!
-                        </WorkGridItem>
-                    </Section>
-                </SimpleGrid>
-            </Container>
-        </Layout>
-    )
+  return (
+    <Layout title="Experience">
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Experience
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="caarsy"
+              title="Caarsy(startup)"
+              thumbnail={caarsyThumbnail}
+            >
+              India{"'"}s first inventory management software for second hand
+              vehicle dealerships.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="scouttTech"
+              title="Scoutt-Tech"
+              thumbnail={scouttTechThumbnail}
+            >
+              Helping recruiters from early startups to get the best developers
+              across the country!
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="predli"
+              title="Predli"
+              thumbnail={predliThumbnail}
+            >
+              Delivering services to startups/companies!
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  )
 }
 
 export default Experience
---
+++ pages/works/predli.js
@@ -0,0 +1,28 @@
+import { Container, Badge } from '@chakra-ui/react'
+import { Title } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Predli = () => {
+  return (
+    <Layout title="Predli">
+      <Container>
+        <Title>
+          Predli: Full Stack Engineer<Badge>Dec 2021-</Badge>
+        </Title>
+        <P>
+          Predli is a global decentralized firm at the forefront of emerging
+          technologies. Built by leading experts and researchers from the tech,
+          business and cognitive science fields.
+          <br />
+          My work here consists of working on different projects for our clients
+          and delivering them their product with utmost perfection. I work on
+          different technologies from frontend frameworks to cloud development
+          and devops practices.
+        </P>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Predli
